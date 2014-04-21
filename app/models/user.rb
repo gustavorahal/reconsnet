1b99098de8aa9e68530c389ed8acebcaad8c3d7e
Gustavo Rahal <grahal@vnet.linux.ibm.com>
2014-04-21 20:59:24 -0300

    Atualização das telas de login/logout e melhorias gerais

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,8 @@ class User < ActiveRecord::Base
 
   ROLES = %w(Admin Financeiro Voluntariado)
 
+  validates_presence_of :name
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
