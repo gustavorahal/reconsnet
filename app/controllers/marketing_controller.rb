0080b2f9ef18d2772caa36477cde20c6b9301b0d
Gustavo Rahal <grahal@br.ibm.com>
2014-09-18 11:53:04 -0300

    Não listar pessoas com emails vazios

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -4,7 +4,7 @@ class MarketingController < ApplicationController
   after_action :verify_authorized
 
   def index
-    @people = Person.where(marketing: true).order('LOWER(name)')
+    @people = Person.where(marketing: true).where("email <> ''").order('LOWER(name)')
     authorize @people
   end
 
