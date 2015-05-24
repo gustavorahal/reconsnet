6b876086001e34c270eba364e7e261f884f78ee3
Gustavo Rahal <grahal@me.com>
2015-05-24 12:38:47 -0300

    Ajustes

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -1,8 +1,7 @@
 class TinymceAssetsController < ApplicationController
   def create
     assetable_id, assetable_type = params[:hint].split(',')
-    asset = Asset.new(name: params[:file].original_filename,
-                      file: params[:file], assetable_id: assetable_id, assetable_type: assetable_type)
+    asset = Asset.new(file: params[:file], assetable_id: assetable_id, assetable_type: assetable_type)
     asset.save!
 
     render json: {
