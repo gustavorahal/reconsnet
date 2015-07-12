759d480255150b973da7e62b7861e1d365cc15e3
Gustavo Rahal <grahal@me.com>
2015-07-12 18:48:09 -0300

    Ajustar permissões para participantes

--- app/policies/participation_policy.rb
+++ app/policies/participation_policy.rb
@@ -1,11 +1,37 @@
 class ParticipationPolicy < ApplicationPolicy
 
+
+  # Não voluntários não podem ver as participações
+
   def index?
     (user.is_admin? or user.is_volunteer?) if user
   end
 
   def show?
     (user.is_admin? or user.is_volunteer?) if user
+  end
+
+
+  # Apenas Admin e Gestor de eventos podem editar/modificar
+
+  def create?
+    user.is_event_manager? or user.is_admin? if user
+  end
+
+  def new?
+    user.is_event_manager? or user.is_admin? if user
+  end
+
+  def update?
+    user.is_event_manager? or user.is_admin? if user
+  end
+
+  def edit?
+    user.is_event_manager? or user.is_admin? if user
+  end
+
+  def destroy?
+    user.is_event_manager? or user.is_admin? if user
   end
 
 end
--- spec/policies/participation_policy_spec.rb
+++ spec/policies/participation_policy_spec.rb
@@ -1,26 +1,21 @@
 require 'rails_helper'
 
-describe EventPolicy do
-  subject { EventPolicy.new(user, event) }
+describe ParticipationPolicy do
+  subject { ParticipationPolicy.new(user, participation) }
 
-  let(:event) { create(:event) }
+  let(:participation) { create(:participation) }
 
 
   context 'para um visitante' do
     let(:user) { nil }
 
-    it { should     permit(:index)      }
-    it { should     permit(:show)       }
-
+    it { should_not permit(:index)      }
+    it { should_not permit(:show)       }
     it { should_not permit(:create)     }
     it { should_not permit(:new)        }
     it { should_not permit(:update)     }
     it { should_not permit(:edit)       }
     it { should_not permit(:destroy)    }
-    it { should_not permit(:emails)     }
-    it { should_not permit(:attendance) }
-    it { should_not permit(:archive)    }
-    it { should_not permit(:unarchive)  }
   end
 
 
@@ -29,17 +24,12 @@ describe EventPolicy do
 
     it { should     permit(:index)      }
     it { should     permit(:show)       }
-    it { should     permit(:emails)     }
-    it { should     permit(:attendance) }
 
     it { should_not permit(:create)     }
     it { should_not permit(:new)        }
     it { should_not permit(:update)     }
     it { should_not permit(:edit)       }
     it { should_not permit(:destroy)    }
-
-    it { should_not permit(:archive)    }
-    it { should_not permit(:unarchive)  }
   end
 
 
@@ -53,10 +43,6 @@ describe EventPolicy do
     it { should permit(:update)     }
     it { should permit(:edit)       }
     it { should permit(:destroy)    }
-    it { should permit(:emails)     }
-    it { should permit(:attendance) }
-    it { should permit(:archive)    }
-    it { should permit(:unarchive)  }
   end
 
 
