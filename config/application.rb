5efdd6b249f9adf4004a0ef22871ebe5e1511707
Gustavo Rahal <grahal@me.com>
2016-01-29 17:01:30 -0200

    Ajuste de layout pagina inicial e do menu para telas pequenas

--- config/application.rb
+++ config/application.rb
@@ -31,10 +31,6 @@ module ReconsNet
     # Be sure to have the adapter's gem in your Gemfile and follow
     # the adapter's specific installation and deployment instructions.
     config.active_job.queue_adapter = :sidekiq
-
-    config.to_prepare do
-      Devise::SessionsController.layout "devise"
-      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
-    end
+    
   end
 end
